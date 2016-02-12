b3995c751af60dfb39d3412b2256106d90c53295
Paul <paulalexrees@gmail.com>
2016-02-12 17:49:45 +0000

    Refactored with uniq.count

--- lib/whichtriangle.rb
+++ lib/whichtriangle.rb
@@ -7,14 +7,10 @@ def type_of_triangle(a,b,c)
 # invalid check
 return "invalid" if arr[0]+arr[1] <= arr[2]
 
-# Equ check
-#return "Equilateral" if arr <=> arr
-# Isos check
-arr.each_cons(3){|a,b,c|
-  return "Equilateral" if a==b && a==c
-  return "Isosceles" if (a==b && a!=c) || (b==c) && (b!=a)
-  return "Scalene" if a!=b && a!=c && b!=c
-}
+# returns index of first array provided by number of similar values in arr
+["Equilateral","Isosceles","Scalene"][arr.uniq.count - 1]
+
+
 end
 
-puts type_of_triangle(1,3,3)
+#puts type_of_triangle(1,3,3)
